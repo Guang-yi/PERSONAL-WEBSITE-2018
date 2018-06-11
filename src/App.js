607e808ade918ddb82c578a3278699bb1b29fedc
Michael Lee <gl83@duke.edu>
2018-06-10 18:34:19 -0700

    Basic Navigation and Layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import 'antd/dist/antd.css'
+
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer } = Layout;
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
+              >
+              <Menu.Item key="1">Home</Menu.Item>
+              <Menu.Item key="2">Bio</Menu.Item>
+              <Menu.Item key="3">Gallery</Menu.Item>
+              <Menu.Item key="4">Instagram</Menu.Item>
+              <Menu.Item key="5">Announcements</Menu.Item>
+              <Menu.Item key="6">Connect</Menu.Item>
+              <Menu.Item key="7">Contact</Menu.Item>
+            </Menu>
+        </Header>
+          <Content style={{ padding: '0 50px' }}>
+            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+              Mike Lee Design Inc. ©2018 Created w/React, Ant.Design, Heroku
+          </Footer>
+      </Layout>
     );
   }
 }
